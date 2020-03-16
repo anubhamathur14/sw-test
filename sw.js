df5f5bb127888b48c396a2951d6f5ca1ed5f5b98
Anubha Mathur <anubmat@microsoft.com>
2020-03-16 16:15:44 -0700

    remove a file

--- sw.js
+++ sw.js
@@ -9,7 +9,6 @@ self.addEventListener('install', function(event) {
         '/sw-test/image-list.js',
         '/sw-test/star-wars-logo.jpg',
         '/sw-test/gallery/bountyHunters.jpg',
-        '/sw-test/gallery/myLittleVader.jpg',
         '/sw-test/gallery/snowTroopers.jpg'
       ]);
     })
