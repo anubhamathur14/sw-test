a8f1649f6f87134cb07d8d4a2afd7a70e71b80c0
Anubha Mathur <anubmat@microsoft.com>
2020-03-17 17:49:27 -0700

    50

--- sw.js
+++ sw.js
@@ -18,8 +18,8 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   if (event.request.url === "https://anubhamathur14.github.io/sw-test/gallery/snowTroopers.jpg") {
     setTimeout(() => {
-      return Promise.resolve();
-    }, 80);
+      return caches.match('/sw-test/gallery/myLittleVader.jpg');
+    }, 50);
   } else {
   event.respondWith(caches.match(event.request).then(function(response) {
     // caches.match() always resolves
