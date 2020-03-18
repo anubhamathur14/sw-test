3c2904892369cd3cb0483a2a2e814ebd7d666c08
Anubha Mathur <anubmat@microsoft.com>
2020-03-18 14:57:56 -0700

    fix error

--- sw.js
+++ sw.js
@@ -24,31 +24,30 @@ self.addEventListener('fetch', function(event) {
     event.waitUntil(
       caches.open('v1').then(function(cache) {
         setTimeout(() => {
-          return caches.match('/sw-test/gallery/alaska.jpg');
+          event.respondWith(caches.match(event.request).then(function(response) {
+            // caches.match() always resolves
+            // but in case of success response will have value
+            if (response !== undefined) {
+              return response;
+            } else {
+              return fetch(event.request).then(function (response) {
+                // response may be used only once
+                // we need to save clone to put one copy in cache
+                // and serve second one
+                let responseClone = response.clone();
+                
+                caches.open('v1').then(function (cache) {
+                  cache.put(event.request, responseClone);
+                });
+                return response;
+              }).catch(function () {
+                return caches.match('/sw-test/gallery/myLittleVader.jpg');
+              });
+            }
+          }));
         }, 250);
         console.log("Wait complete until waitUntil")
       })
     );
-    event.respondWith(caches.match(event.request).then(function(response) {
-      // caches.match() always resolves
-      // but in case of success response will have value
-      if (response !== undefined) {
-        return response;
-      } else {
-        return fetch(event.request).then(function (response) {
-          // response may be used only once
-          // we need to save clone to put one copy in cache
-          // and serve second one
-          let responseClone = response.clone();
-          
-          caches.open('v1').then(function (cache) {
-            cache.put(event.request, responseClone);
-          });
-          return response;
-        }).catch(function () {
-          return caches.match('/sw-test/gallery/myLittleVader.jpg');
-        });
-      }
-    }));
   }
 });
