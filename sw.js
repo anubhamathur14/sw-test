97f430109a46e151226b1dd04821f1174797672e
Anubha Mathur <anubmat@microsoft.com>
2020-04-04 02:05:13 -0700

    swap images

--- sw.js
+++ sw.js
@@ -105,7 +105,7 @@ self.addEventListener('fetch', function(event) {
         return resp;
       })
     );
-  } else if (event.request.url === "https://anubhamathur14.github.io/sw-test/gallery/img_1.jpg") {
+  } else if (event.request.url === "https://anubhamathur14.github.io/sw-test/gallery/img_2.jpg") {
     event.waitUntil(
       caches.open('v1').then(() => {
         return timeConsumingFunction().then(() => {
@@ -127,7 +127,7 @@ self.addEventListener('fetch', function(event) {
         return resp;
       })
     );
-  } else if (event.request.url === "https://anubhamathur14.github.io/sw-test/gallery/img_2.jpg") {
+  } else if (event.request.url === "https://anubhamathur14.github.io/sw-test/gallery/img_1.jpg") {
     event.respondWith(
       caches.match(event.request).then((resp) => {
         return resp;
